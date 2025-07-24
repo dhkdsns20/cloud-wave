e2e852832e2ecd2c23eff7abe39d26dc1c75c843
Workshop Public Cloud <cj-cloud-wave@workshops.aws>
2025-07-24 07:51:45 +0000

    Forth

--- scripts/stop_streamlit.sh
+++ scripts/stop_streamlit.sh
@@ -13,17 +13,19 @@
 
 # exit 0
 
+
 #!/bin/bash
 
 APP_NAME="streamlit"
-APP_PID=$(pgrep "$APP_NAME")
+APP_PID=$(pgrep -u root "$APP_NAME")
 
 if [ -n "$APP_PID" ]; then
   echo "Killing process: $APP_PID"
-  kill -9 "$APP_PID" || echo "Failed to kill process $APP_PID"
+  sudo kill -9 "$APP_PID" || echo "Failed to kill process $APP_PID"
   sleep 2
 else
   echo "No $APP_NAME process is running"
 fi
 
 exit 0
+
