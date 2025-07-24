793c1b5d1570dfe9cc8e2490148a72803a120476
Workshop Public Cloud <cj-cloud-wave@workshops.aws>
2025-07-24 08:17:37 +0000

    제발

--- scripts/stop_streamlit.sh
+++ scripts/stop_streamlit.sh
@@ -1,30 +1,14 @@
-# #!/bin/bash
-# #APP_Name=streamlit
-# APP_PID=$(pgrep streamlit)
-
-# if [ -z "$APP_PID" ];
-# then
-#   echo "Application is not running"
-# else
-#   echo "Kill -9 $APP_PID"
-#   kill -9 "$APP_PID"
-#   sleep 5
-# fi
-
-# exit 0
-
-
 #!/bin/bash
+#APP_Name=streamlit
+APP_PID=$(pgrep streamlit)
 
-APP_NAME="streamlit"
-APP_PID=$(pgrep -u root "$APP_NAME")
-
-if [ -n "$APP_PID" ]; then
-  echo "Killing process: $APP_PID"
-  sudo kill -9 "$APP_PID" || echo "Failed to kill process $APP_PID"
-  sleep 2
+if [ -z $APP_PID ];
+then
+  echo "Application is not running"
 else
-  echo "No $APP_NAME process is running"
+  echo "Kill -9 $APP_PID"
+  kill -9 "$APP_PID"
+  sleep 5
 fi
 
 exit 0
